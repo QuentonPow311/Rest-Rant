0134c7b381cbe4a941c45cd87bb888377c306de2
QuentonPow311 <quenpowell@gmail.com>
2022-07-24 10:15:00 -0400

    update

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
+//Modules and Globals
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
+//Express Settings
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+
+//Controllers and Routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.render('Home')
 })
 
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
 
+//Listen for connections 
 app.listen(process.env.PORT)
 
 
