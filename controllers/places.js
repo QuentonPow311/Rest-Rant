95f4e5bc86e8d6e549763b34f441eed00fe171ba
QuentonPow311 <quenpowell@gmail.com>
2022-08-09 17:13:15 -0400

    update

--- controllers/places.js
+++ controllers/places.js
@@ -9,7 +9,7 @@ router.get('/new', (req, res) => {
     console.log(req.body)
     if (!req.body.pic) {
       // Default image if one is not provided
-      req.body.pic = 'http://placekitten.com/400/400'
+      req.body.pic = "/images/chia-fruit-drink.jpg"
     }
     if (!req.body.city) {
       req.body.city = 'Anytown'
--- views/default.jsx
+++ views/default.jsx
@@ -1,37 +1,33 @@
-const React = require('react')
+const React = require("react");
 // const Def = require('../default')
-function Def (html) {
-    return (
-        <html>
-            <head>
-                <title>Title</title>
-               
-                <link rel="stylesheet" href="/css/style.css" />
-            </head>
-            <body>
+function Def(html) {
+  return (
+    <html>
+      <head>
+        <title>Title</title>
 
+        <link rel="stylesheet" href="/css/style.css" />
+      </head>
+      <body>
+      <nav>
+  <ul>
+    <li>
+      <a href="/">Home</a>
+    </li>
+    <li>
+      <a href="/places">Places</a>
+    </li>
+    <li>
+      <a href="/places/new">Add Place</a>
+    </li>
+  </ul>
+</nav>
 
+        {html.children}
 
-                {html.children}
-            <div class="content">
-  {/* <h1>Sticky Footer with Flexbox</h1> */}
-  <p><button id="add">Add Content</button></p>
-</div>
+      </body>
+    </html>
+  );
+}
 
-<footer class="footer">
-<ul>
-    Instagram:Jondoe33_
-    
-Twitter:Jondoe33
-LinkedIN:JonDoe55__
-</ul>
-
-
-</footer>
-            </body>
-        </html>
-    )
-  }
-  
-
-module.exports = Def 
+module.exports = Def;
