149bef0d9e8c947b959870dc8a7bb78e91c63f35
QuentonPow311 <quenpowell@gmail.com>
2022-08-09 16:48:44 -0400

    update

--- views/default.jsx
+++ views/default.jsx
@@ -9,7 +9,25 @@ function Def (html) {
                 <link rel="stylesheet" href="/css/style.css" />
             </head>
             <body>
+
+
+
                 {html.children}
+            <div class="content">
+  {/* <h1>Sticky Footer with Flexbox</h1> */}
+  <p><button id="add">Add Content</button></p>
+</div>
+
+<footer class="footer">
+<ul>
+    Instagram:Jondoe33_
+    
+Twitter:Jondoe33
+LinkedIN:JonDoe55__
+</ul>
+
+
+</footer>
             </body>
         </html>
     )
